2865409b3264bfae1009fdbd303f9da6afc9d8a9
dbaldew <dbaldew@gmail.com>
2021-09-15 16:01:04 +0200

    header + button

--- src/App.js
+++ src/App.js
@@ -7,26 +7,35 @@ import ijsblokjes from './assets/ijsblokjes.jpg'
 
 function App() {
     return (
-        <main>
-            <Product
-                img = {citroenen}
-                alt = "citroenen.jpg"
-                title = "Citroenen"
-                description = "Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
-            />
-            <Product
-                img = {limoenen}
-                alt = "limoenen.jpg"
-                title = "Citroenen"
-                description = "Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
-            />
-            <Product
-                img = {ijsblokjes}
-                alt = "ijsblokjes.jpg"
-                title = "Citroenen"
-                description = "Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
-            />
-       </main>
+        <>
+            <header>
+                <h1>Fruit Perfection</h1>
+                <button type="button" onClick= {() => console.log("jij wil shoppen!")}
+
+                >Shop nu</button>
+            </header>
+            <main>
+                <Product
+                    img={citroenen}
+                    alt="citroenen.jpg"
+                    title="Citroenen"
+                    description="Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
+                />
+                <Product
+                    img={limoenen}
+                    alt="limoenen.jpg"
+                    title="Citroenen"
+                    description="Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
+                />
+                <Product
+                    img={ijsblokjes}
+                    alt="ijsblokjes.jpg"
+                    title="Citroenen"
+                    description="Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
+                />
+            </main>
+        </>
+
     );
 }
 
