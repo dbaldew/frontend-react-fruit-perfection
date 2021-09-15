25c9c146cbac04dd4968b68c782e2c253129dc11
dbaldew <dbaldew@gmail.com>
2021-09-15 16:30:09 +0200

    nav & shoppingcart

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import './App.css';
 import Product from "./Components/Product";
-import citroenen from './assets/citroenen.jpeg'
-import limoenen from './assets/limoenen.png'
-import ijsblokjes from './assets/ijsblokjes.jpg'
+import citroenen from './assets/citroenen.jpeg';
+import limoenen from './assets/limoenen.png';
+import ijsblokjes from './assets/ijsblokjes.jpg';
+import {ReactComponent as ShoppingCart} from './assets/winkelmandje.svg';
 
 function App() {
     return (
+
         <>
+            <nav>
+                <ul>
+                    <li><a href="/">Shop</a></li>
+                    <li><a href="/">Ons verhaal</a></li>
+                    <li><a href="/">Blog</a></li>
+                </ul>
+                <ShoppingCart className = "shopping-cart-icon"/>
+            </nav>
             <header>
                 <h1>Fruit Perfection</h1>
                 <button type="button" onClick= {() => console.log("jij wil shoppen!")}
-
                 >Shop nu</button>
             </header>
             <main>
